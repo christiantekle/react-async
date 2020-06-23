abd968253dba6d28ce2efa2aa8bdac7520147fc9
christiantekle <christiantekle@users.noreply.github.com>
2020-06-24 00:14:49 +0400

    links added to the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import Header from './components/Header'
-import { Container } from '@material-ui/core'
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Users from "./components/Users";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/About";
+import { Container } from "@material-ui/core";
+import "./App.css";
+import axios from "axios";
 
-
-function App() {
-  return (
-    <Container>
-      <Header />
-    </Container>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Header />
+          <Route exact path="/" component={Users} />
+          <Route path="/About" component={About} />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
- import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
- const Header = () => {
-    return (
-        <di>
-            <header>
-               <h1>Simple react app</h1>
-            </header>
-        </di>
-    )
- }
+const Header = () => {
+  return (
+    <di>
+      <header>
+        <h1>Simple react app</h1>
+        <Link to="/">Home</Link> | <Link to="/About">About</Link>
+      </header>
+    </di>
+  );
+};
 
- export default Header;
+
+export default Header;
